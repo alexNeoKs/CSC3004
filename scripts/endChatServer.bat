333451182a6fada12f5ce96c5b0ae44c8ee666cb
Kee Boon Hwee <2717921K@student.gla.ac.uk>
2023-06-21 11:49:21 +0800

    update

--- scripts/endChatServer.bat
+++ scripts/endChatServer.bat
@@ -1,6 +1,7 @@
 docker rmi chatserver:latest
 kubectl delete ingress    flask-ingress
-kubectl delete secret     chatserver-tls
+kubectl delete secret     self-tls
+kubectl delete secret     ca-secret
 kubectl delete service    flask-service
 kubectl delete deployment flask-app
 minikube stop
--- scripts/startChatServer.bat
+++ scripts/startChatServer.bat
@@ -4,22 +4,22 @@ minikube addons enable ingress
 START kubectl port-forward -n ingress-nginx service/ingress-nginx-controller 8080:80
 START kubectl port-forward -n ingress-nginx service/ingress-nginx-controller  443:443
 
-minikube image build -t chatserver .
+minikube image build -t chatserver ../app/
 minikube image ls
 
-kubectl apply -f deployment.yaml
+kubectl apply -f ../kubernetes/deployment.yaml
 kubectl get deployment flask-app --output=wide
 
-kubectl apply -f service.yaml
+kubectl apply -f ../kubernetes/service.yaml
 kubectl get service flask-service --output=wide
 
-kubectl create secret tls self-tls --key server.key --cert server.crt
-kubectl create secret generic ca-secret --from-file=ca.crt=ca.crt
+kubectl create secret tls     self-tls  --key ../servercert/server.key --cert ../servercert/server.crt
+kubectl create secret generic ca-secret --from-file=ca.crt=../cacert/ca.crt
 
 kubectl get secret --output=wide
 kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
-kubectl apply -f ingress.yaml
+kubectl apply -f ../kubernetes/ingress.yaml
 kubectl get ingress flask-ingress --output=wide
 
 START minikube tunnel
-START curl -v --cacert ca.crt --key client.key --cert client.crt https://chatserver.com/
+START curl -k -v --cacert ../cacert/ca.crt --key ../clientcert/client.key --cert ../clientcert/client.crt https://chatserver.com/
